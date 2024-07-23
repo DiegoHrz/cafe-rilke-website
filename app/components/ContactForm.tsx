d0260a8e2be94c118d479d45e529ad18fd68664b
DiegoHrz <hernandezwdiego@gmail.com>
2024-07-22 23:53:30 -0500

    Add contact form

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -37,8 +37,8 @@ export function ContactForm() {
   };
 
   return (
-    //  GRADIENTE  bg-gradient-to-br from-pink-500 via-red-500 to-yellow-500 
-    <div className="w-full rounded-[16px] bg-gradient-to-br from-[#F5F5F5] via-[#F5F5F5] to-gray-300 py-[56px] px-[32px] text-center lg:my-[60px] lg:px-[324px] lg:py-[89px]">
+    //  GRADIENTE  bg-gradient-to-br from-pink-500 via-red-500 to-yellow-500
+    <div className="w-full bg-gradient-to-br from-[#F5F5F5] via-[#F5F5F5] to-gray-300 py-[56px] px-[32px] text-center lg:my-[60px] lg:px-[200px] lg:py-[89px]">
       <h1 className=" text-4xl font-medium lg:text-[56px] leading-[64px] ">
         Contactanos
       </h1>
@@ -46,28 +46,29 @@ export function ContactForm() {
         Cualquier sugerencia, escribanos
       </p>
 
-      <div className="w-full lg:w-9/12 p-4 lg:p-8 mt-2 rounded-2xl ">
+      <div className="w-full  lg:p-8 mt-2 rounded-2xl flex justify-center">
         <form
           action=""
           id="form"
           onSubmit={handleSubmit(onSubmit)}
           autoComplete=""
+          className="max-w-[60rem] w-full md:w-7/12 lg:w-full xl:w-3/5 flex flex-col gap-6"
         >
-          <div className="flex flex-col-reverse my-3">
+          <div className="flex flex-col-reverse">
             <input
               type="text"
               id="name"
-              placeholder="Ingresa tu nombre completo"
+              placeholder="Nombre"
               {...register("nombre", {
                 required: true,
                 pattern: /^[a-zA-Z]+(?: [a-zA-Z]+)*$/,
               })}
-              className="border rounded p-1 text-sm placeholder-zinc-300"
+              className="border p-3 text-sm h-10 placeholder:text-[#757575]/70 placeholder:italic placeholder:font-[100] placeholder:text-base rounded-md"
             />
 
             <label htmlFor="name" className=" flex text-sm">
-              <div className="mr-1 text-slate-600">Nombre </div>{" "}
-              <div className="text-slate-600"> Completo</div>
+              <div className="mr-1 text-slate-600"> </div>{" "}
+              <div className="text-slate-600"> </div>
               <span className="w-full flex justify-end items-center">
                 {errors.nombre?.type === "required" && (
                   <span className="text-red-600 text-[12px] ">
@@ -83,20 +84,19 @@ export function ContactForm() {
             </label>
           </div>
 
-          <div className="flex flex-col-reverse my-3">
+          <div className="flex flex-col-reverse">
             <input
               type="email"
               id="email"
-              placeholder="Direccion de Correo Electronico"
+              placeholder="Email"
               {...register("correo", {
                 required: true,
                 pattern: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
               })}
-              className="border rounded p-1 text-sm placeholder-zinc-300"
+              className="border p-3 text-sm h-10 placeholder:text-[#757575]/70 placeholder:italic placeholder:font-[100] placeholder:text-base rounded-md"
             />
 
             <label htmlFor="email" className="flex text-sm text-slate-600">
-              Correo{" "}
               <span className="w-full flex justify-end items-center">
                 {errors.correo?.type === "pattern" && (
                   <span className="text-red-600 text-[12px]">
@@ -112,7 +112,7 @@ export function ContactForm() {
             </label>
           </div>
 
-          <div className="flex flex-col-reverse my-3">
+          {/* <div className="flex flex-col-reverse my-3">
             <input
               type="text"
               id="telephone"
@@ -121,11 +121,10 @@ export function ContactForm() {
                 required: true,
                 pattern: /^(\+\d{1,2}|(\(\d{1,4}\)))?\d{5,15}$/,
               })}
-              className="border rounded p-1 text-sm placeholder-zinc-300"
+              className="border p-3 text-sm h-10 placeholder:text-[#757575]/70 placeholder:italic placeholder:font-[100] placeholder:text-base"
             />
 
             <label htmlFor="telephone" className="flex text-sm text-slate-600">
-              Celular{" "}
               <span className="w-full flex justify-end">
                 {errors.celular?.type === "required" && (
                   <span className="text-red-600 text-[12px]">
@@ -134,21 +133,20 @@ export function ContactForm() {
                 )}
               </span>
             </label>
-          </div>
+          </div> */}
 
-          <div className="flex flex-col-reverse my-3">
+          <div className="flex flex-col-reverse">
             <input
               type="text"
               id="asunto"
-              placeholder="Asunto del mensaje"
+              placeholder="Asunto"
               {...register("asunto", {
                 required: true,
               })}
-              className="border rounded p-1 text-sm placeholder-zinc-300 "
+              className="border p-3 text-sm h-10 placeholder:text-[#757575]/70 placeholder:italic placeholder:font-[100] placeholder:text-base rounded-md"
             />
 
             <label htmlFor="asunto" className="flex  text-sm text-slate-600">
-              Asunto
               <span className="w-full flex justify-end">
                 {errors.asunto?.type === "required" && (
                   <span className="text-red-600 text-[12px]">
@@ -159,22 +157,21 @@ export function ContactForm() {
             </label>
           </div>
 
-          <div className="flex flex-col-reverse my-3">
+          <div className="flex flex-col-reverse">
             <textarea
               id="mensaje"
               cols={1}
               rows={4}
-              placeholder="Escribenos todos los detalles de tu mensaje"
+              placeholder="Mensaje"
               {...register("mensaje", {
                 required: true,
               })}
-              className="border rounded p-1 text-sm placeholder-zinc-300"
+              className="border p-3 text-sm placeholder:text-[#757575]/70 placeholder:italic placeholder:font-[100] placeholder:text-base rounded-md"
             ></textarea>
 
             <label htmlFor="mensaje" className="flex text-sm text-slate-600">
-              Mensaje{" "}
               <span className="w-full flex justify-end items-center">
-                {errors.asunto?.type === "required" && (
+                {errors.mensaje?.type === "required" && (
                   <span className="text-red-600 text-[12px]">
                     El Mensaje es requerido{" "}
                   </span>
@@ -187,7 +184,7 @@ export function ContactForm() {
             <input
               type="submit"
               value="Enviar"
-              className="cursor-pointer border rounded-md w-full lg:w-auto text-white "
+              className="cursor-pointer border rounded-md w-full lg:w-auto text-white bg-rilke-red px-4 py-2 italic  "
             />
           </div>
         </form>
