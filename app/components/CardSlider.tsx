cba9b725207d7d6b025ee086a704d23f798ffc55
DiegoHrz <hernandezwdiego@gmail.com>
2024-08-05 14:14:33 -0500

    Add removed * changes

--- app/components/CardSlider.tsx
+++ app/components/CardSlider.tsx
@@ -1,5 +1,4 @@
 'use client'
-import React, { useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
@@ -16,24 +15,10 @@ const CardSlider = () => {
     { name: "Richard Wilson", profession: "Product Designer", image: "/images/img-6.jpg" },
   ];
 
-  useEffect(() => {
-    // Add background image to body
-    document.body.style.backgroundImage = "url('/images/bg.jpg')";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundPosition = "center";
-    document.body.style.backgroundColor = "#030728";
 
-    // Cleanup function
-    return () => {
-      document.body.style.backgroundImage = "";
-      document.body.style.backgroundRepeat = "";
-      document.body.style.backgroundPosition = "";
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
 
   return (
-    <div className="flex items-center justify-center min-h-screen font-montserrat">
+    <div className="flex items-center justify-center min-h-screen  bg-gray-500">
       <div className="container max-w-7xl mx-auto px-4">
         <Swiper
           modules={[Pagination, Navigation]}
@@ -54,7 +39,7 @@ const CardSlider = () => {
         >
           {users.map((user, index) => (
             <SwiperSlide key={index} className="card-item p-9 flex flex-col items-center justify-center rounded-lg backdrop-blur-md bg-white/20 border border-white/50">
-              <img src={user.image} alt={user.name} className="w-40 h-40 rounded-full mb-10 border-3 border-white p-1" />
+              <img src={user.image} alt={user.name} className="w-36 h-36 rounded-full mb-10 border-3 border-white p-1" />
               <h2 className="text-white text-xl font-semibold">{user.name}</h2>
               <p className="text-gray-200 text-lg font-medium mt-3 mb-10">{user.profession}</p>
               <button className="text-xl px-9 py-2.5 text-[#030728] rounded-md font-medium cursor-pointer bg-white border border-transparent transition-all duration-200 hover:bg-white/10 hover:border-white hover:text-white">
