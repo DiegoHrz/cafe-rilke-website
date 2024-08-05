aa4ff24b725392e4383697c53161e5d1f507025d
DiegoHrz <hernandezwdiego@gmail.com>
2024-08-04 22:29:56 -0500

    Add galleryContainer display for safari 0

--- app/components/GalleryContainer.tsx
+++ app/components/GalleryContainer.tsx
@@ -4,21 +4,25 @@ import ImageContainer from "./ImageContainer";
 const GalleryContainer = () => {
   return (
     <div className="max-w-6xl mx-auto flex flex-col justify-center gap-4 px-5 md:px-9">
-      <div className="grid gap-4 grid-cols-3 md:grid-cols-6 place-content-center">
-        <ImageContainer image="ravioli-yuca" />
-        <ImageContainer image="tartaleta-fresa" />
-        <ImageContainer image="tacu-tacu" />
-        <ImageContainer image="causa" />
-        <ImageContainer image="cafe-rilke" />
-        <ImageContainer image="estofado-vino" />
-      </div>
-      <div className="grid gap-4 grid-cols-3 md:grid-cols-6 place-content-center">
-        <ImageContainer image="biblioteca" />
-        <ImageContainer image="garden" />
-        <ImageContainer image="torta-chocolate" />
-        <ImageContainer image="pie-limon" />
-        <ImageContainer image="desayuno" />
-        <ImageContainer image="panadera" />
+      <div className="flex flex-wrap justify-center gap-4">
+        {[
+          "ravioli-yuca",
+          "tartaleta-fresa",
+          "tacu-tacu",
+          "causa",
+          "cafe-rilke",
+          "estofado-vino",
+          "biblioteca",
+          "garden",
+          "torta-chocolate",
+          "pie-limon",
+          "desayuno",
+          "panadera"
+        ].map((image, index) => (
+          <div key={index} className="w-[calc(33.333%-1rem)] md:w-[calc(16.666%-1rem)]">
+            <ImageContainer image={image} />
+          </div>
+        ))}
       </div>
     </div>
   );
