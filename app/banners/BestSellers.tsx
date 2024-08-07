9edc30102e87fce1e51d443db14808978d47ec81
DiegoHrz <hernandezwdiego@gmail.com>
2024-08-06 21:14:00 -0500

    add

--- app/banners/BestSellers.tsx
+++ app/banners/BestSellers.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import CardSlider from "../components/CardSlider";
+import { GiLaurelCrown } from "react-icons/gi";
 
 const BestSellers = () => {
   return (
-    <div className="pt-16">
-      <div className="text-center">
+    <div className="">
+      <div className="text-center py-20">
         <h1 className="py-3 md:py-1 text-xs font-semibold text-[#172026] lg:text-xs lg:leading-7 tracking-[0.25em] md:tracking-[0.15em] font-rilke-monecias">
           DESDE EL BOSQUE DE DIANA
         </h1>
+        <div className="flex justify-center">
+          <GiLaurelCrown size={40} className="text-rilke-red" />
+        </div>
         <h1 className=" text-2xl  text-[#172026] lg:text-[42px] lg:leading-[58px] text-center pb-10 font-rilke-monecias">
           Los BestSellers
         </h1>
--- app/components/CardSlider.tsx
+++ app/components/CardSlider.tsx
@@ -41,47 +41,61 @@ const CardSlider = () => {
   ];
 
   return (
-    <div className="flex items-center justify-center   py-12">
+    <div className="max-w-6xl mx-auto flex flex-col justify-center gap-4 px-5 md:px-9">
       <Swiper
         modules={[Pagination, Navigation]}
         loop={true}
         grabCursor={true}
-        spaceBetween={70}
-        slidesPerView={"auto"}
+        spaceBetween={20}
+        slidesPerView={3}
         centeredSlides={true}
-        // initialSlide={1}
         pagination={{ clickable: true, dynamicBullets: true }}
         navigation={{
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
         }}
         breakpoints={{
-          0: { slidesPerView: 3 },
-          768: { slidesPerView: 5 },
+          0: { slidesPerView: 3, spaceBetween: 1 },
+          768: { slidesPerView: 5, spaceBetween: 15 },
         }}
-        className="slider overflow-visible max-w-7xl"
+        className="slider overflow-visible w-full flex flex-wrap justify-center gap-4"
       >
         {users.map((user, index) => (
+          // <div
+          //   key={index}
+          //   className="w-[calc(33.333%-1rem)] md:w-[calc(16.666%-1rem)] border-red-500 border-4 aspect-square bg-red-500"
+          // >
           <SwiperSlide
             key={index}
-            className="card-item p-9 my-20 flex flex-col items-center justify-center rounded-lg backdrop-blur-md bg-white border  relative"
-            style={{ width: "300px" }} // Fixed width for each slide
+            className="card-item  flex flex-col items-center justify-center rounded-lg backdrop-blur-md bg-white  relative border-red-500 border-4 w-[calc(33.333%-1rem)] md:w-[calc(16.666%-1rem)] "
+            //   style={{
+            //     aspectRatio: '1/1.3'
+            // }}
           >
-            <img
-              src={user.image}
-              alt={user.name}
-              className="w-40 h-40 rounded-full mb-10 border-3 border-white p-1 absolute -top-1 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-            />
-            <h2 className="text-black text-2xl font-semibold mt-10 text-center">
-              {user.name}
-            </h2>
-            <p className="text-[#B3B3B3] text-sm font-medium mt-3 mb-10 text-center">
-              {user.profession}
-            </p>
-            <button className="text-xl px-9 py-2.5 text-white rounded-md font-medium cursor-pointer border border-transparent transition-all duration-200 hover:bg-white/10 hover:border-white hover:text-white w-full font-sans bg-rilke-red animate-custom-pulse">
-              Pidelo
-            </button>
+            <div>
+              <img
+                src={user.image}
+                alt={user.name}
+                className=" rounded-full   p-1  border-green-500 border-4"
+              />
+            </div>
+            <div className="border-2 border-purple-500">
+              <h2 className="text-black text-xs sm:text-sm md:text-base lg:text-lg font-semibold mt-10 sm:mt-14 md:mt-24 text-center px-2">
+                {user.name}
+              </h2>
+            </div>
+            <div className="border-2 border-blue-500">
+              <p className="text-[#B3B3B3] text-xs sm:text-sm font-medium mt-1 sm:mt-2 mb-3 sm:mb-4 md:mb-6 text-center px-2">
+                {user.profession}
+              </p>
+            </div>
+            <div className="border-2 border-yellow-500">
+              <button className="text-xs sm:text-sm md:text-base px-3 sm:px-4 py-1 sm:py-2 text-white rounded-md font-medium cursor-pointer border border-transparent transition-all duration-200 hover:bg-white/10 hover:border-white hover:text-white font-sans bg-rilke-red animate-custom-pulse">
+                Pidelo
+              </button>
+            </div>
           </SwiperSlide>
+          // </div>
         ))}
         <div className="swiper-button-prev text-white mt-[-55px] transition-colors duration-200 hover:text-[#4658ff]"></div>
         <div className="swiper-button-next text-white mt-[-55px] transition-colors duration-200 hover:text-[#4658ff]"></div>
