4057c40a274edca703301202291ad0b5041ff128
DiegoHrz <hernandezwdiego@gmail.com>
2024-08-09 03:05:03 -0500

    Add change in home

--- app/banners/Home.tsx
+++ app/banners/Home.tsx
@@ -3,7 +3,7 @@
 export function Home() {
   return (
     <div className=" h-screen" id="home">
-      <div className="relative flex h-full w-full justify-center bg-fixed bg-parallax-img bg-cover bg-center ">
+      <div className="relative flex h-full w-full justify-center bg-fixed bg-center" style={{ backgroundImage: "url('/assets/home-cafe-2.jpg')" }}>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
           <img
             src="/assets/logo/logo-no-bg/logo-white-no-bg.png"
