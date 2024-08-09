0d74b7dde0dd1cbba15ddb7ccc93e9b13b2bf4bb
DiegoHrz <hernandezwdiego@gmail.com>
2024-08-09 02:43:18 -0500

    Add change in home

--- app/banners/Home.tsx
+++ app/banners/Home.tsx
@@ -1,25 +1,29 @@
 import Image from "next/image";
+import BlurArrow from "../../public/assets/blue-button.svg";
+import Gradient from "../../public/assets/Gradient.svg";
+import HeroImage from "../../public/assets/Image.svg";
+import Google from "../../public/assets/Google.svg";
+import Slack from "../../public/assets/Slack.svg";
+import Truspilot from "../../public/assets/Trustpilot.svg";
+import Cnn from "../../public/assets/CNN.svg";
+import Cluth from "../../public/assets/Clutch.svg";
 import homeCafe from "../../public/assets/home-cafe-2.jpg";
 import logoCafe from "../../public/assets/logo-cafe.png";
 
 export function Home() {
   return (
-    <div className="h-screen" id="home">
-      <div className="relative flex h-full w-full justify-center bg-fixed bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${homeCafe.src})`}}>
+    <div className=" h-screen" id="home">
+      <div className="relative flex h-full w-full justify-center bg-fixed bg-parallax-img bg-cover bg-center bg-no-repeat">
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
-          <Image
+          <img
             src="/assets/logo/logo-no-bg/logo-white-no-bg.png"
-            alt="White Logo"
-            width={300}
-            height={100}
-            className="h-auto w-fit mx-auto px-3 animate-fade-title"
+            alt=""
+            className=" h-full w-fit mx-auto px-3 animate-fade-title"
           />
-          <Image
+          <img
             src="/assets/logo/logo-no-bg/logo-cafe-bistre-no-bg.png"
-            alt="Cafe Logo"
-            width={200}
-            height={50}
-            className="mx-auto mt-4 animate-fade-subtitle"
+            alt=""
+            className=" mx-auto mt-4  animate-fade-subtitle"
           />
         </div>
       </div>
