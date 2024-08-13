bff040b2fb37dcd46d491f45bf118536061ae8dd
DiegoHrz <hernandezwdiego@gmail.com>
2024-08-13 16:09:39 -0500

    Add changes in the pdf viewer page

--- app/Pdf/page.tsx
+++ app/Pdf/page.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-
-
 import Head from "next/head";
 
 const PdfPage = () => {
@@ -8,12 +6,25 @@ const PdfPage = () => {
     <div className="w-screen h-screen">
       <Head>
         <title>Archivo PDF</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <iframe
         src="/assets/menu/carta.pdf"
         style={{ border: "none", width: "100%", height: "100%" }}
         title="Archivo PDF"
       />
+      <style jsx>{`
+        iframe {
+          width: 100%;
+          height: 100%;
+        }
+
+        @media (max-width: 768px) {
+          iframe {
+            height: 80vh;
+          }
+        }
+      `}</style>
     </div>
   );
 };
