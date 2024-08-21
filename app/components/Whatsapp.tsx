bc7d1f8322826c0cc3d2b95854d20841b16d2fda
DiegoHrz <hernandezwdiego@gmail.com>
2024-08-21 02:34:06 -0500

    Add whatsapp component changes

--- app/components/Whatsapp.tsx
+++ app/components/Whatsapp.tsx
@@ -2,9 +2,12 @@ import { FaFacebookF, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 const Whatsapp: React.FC = () => {
   return (
-    <div className="fixed bottom-4 right-4  z-50  flex flex-col justify-center items-center animate-custom-pulse-3 gap-3">
+    <div className="fixed bottom-10 right-3  z-50  flex justify-center items-center animate-custom-pulse-3 gap-3">
       <div className="bg-[#47C755] text-white   text-xl p-2 relative">
-        <p className="font-sans">Pide Ya!</p>
+        <p className="font-sans flex gap-1">
+          <FaWhatsapp color="white" className="" size={30} />
+          Pide Ya!
+        </p>
         <div
           className=" absolute -bottom-3 left-1/2 -translate-x-1/2 -translate-y-1/2  "
           style={{
@@ -16,7 +19,6 @@ const Whatsapp: React.FC = () => {
           }}
         ></div>
       </div>
-      <FaWhatsapp color="#47C755" className="" size={50} />
     </div>
   );
 };
