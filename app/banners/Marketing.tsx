117c8e591a0e46b6747ca338e90d5601d6c7b7b9
DiegoHrz <hernandezwdiego@gmail.com>
2024-09-02 04:01:56 -0500

    Add Marketing banner finished

--- app/banners/Marketing.tsx
+++ app/banners/Marketing.tsx
@@ -5,40 +5,34 @@ import { RiStarSFill } from "react-icons/ri";
 
 const Marketing = () => {
   return (
-    <div className="mx-auto w-[80%] my-10">
-      <div className="w-fit border-4 border-[#AA8A43] rounded-full mx-auto relative">
-        <div className="w-fit border-4 border-[#77421A] rounded-full mx-auto">
-          <div className="flex  flex-col   mx-auto justify-center py-3 md:py-5 px-6   lg:gap-0  landing-container rounded max-h-[18rem] aspect-square   max-w-[18rem] border-4 border-[#DDC47E]">
-            <div className="flex flex-col  text-center md:pb-1 ">
-              <p className="block  font-sans text-3xl md:text-2xl font-medium ">4.9</p>
-              <div className="flex justify-center  ">
-                <RiStarSFill className="text-rilke-red text-2xl" />
-                <RiStarSFill className="text-rilke-red text-2xl" />
-                <RiStarSFill className="text-rilke-red text-2xl" />
-                <RiStarSFill className="text-rilke-red text-2xl" />
-                <RiStarSFill className="text-rilke-red text-2xl" />
-              </div>
+    <div className="mx-auto w-[80%] my-20 ">
+      <div className="w-fit   mx-auto   ">
+        <div className="flex  flex-col   mx-auto justify-center py-3 md:py-5 px-6   lg:gap-0  bg-none rounded-full max-h-[18rem] aspect-square   max-w-[18rem] relative">
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[96%] w-[96%] sm:h-[110%] sm:w-[110%] -z-10 overflow-visible">
+            <img
+              src="/assets/frames/cert1.png"
+              className="w-full h-full object-cover animation-rotation"
+              alt="Rotating certificate"
+            />
+          </div>
 
-              <p className=" font-serif text-2xl md:text-xl">reviews</p>
-            </div>
-            <div className=" text-center  text-[#282727] text-base md:text-base max-w-[30rem]  md:max-w-[50rem] mx-auto  ">
-              <p className=" py-4">
-                &apos;La inauguración del café propició el encuentro del arte,
-                la música y la gastronomía&apos;
-              </p>
-              <a
-                href="https://cosas.pe/vida-social/68622/cafe-rilke/"
-                target="_blank"
-                className="block w-fit mx-auto pb-2"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src="/assets/cosas/cosas.png"
-                  alt=""
-                  className=" h-8 md:h-10 animation-opacity-infinite mx-auto    "
-                />
-              </a>
-            </div>
+          <div className=" text-center  text-black text-base md:text-base px-2 max-w-[13rem]  md:max-w-[50rem] mx-auto font-bold ">
+            <a
+              href="https://cosas.pe/vida-social/68622/cafe-rilke/"
+              target="_blank"
+              className="block w-fit mx-auto pb-1 sm:pb-2"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/assets/cosas/cosas.png"
+                alt=""
+                className=" h-8 md:h-10 animation-opacity-infinite mx-auto    "
+              />
+            </a>
+            <p className=" px-4 text-sm  sm:text-base">
+              &apos;La inauguración del café propició el encuentro del arte, la
+              música y la gastronomía&apos;
+            </p>
           </div>
         </div>
       </div>
@@ -47,3 +41,54 @@ const Marketing = () => {
 };
 
 export default Marketing;
+
+// import React from "react";
+// import { RiStarSFill } from "react-icons/ri";
+
+// // bg-gradient-to-br from-[#ECE2D2] via-[#f1f1f1] to-[#eed9b6]
+
+// const Marketing = () => {
+//   return (
+//     <div className="mx-auto w-[80%] my-10 ">
+//       <div className="w-fit border-4 border-[#AA8A43] rounded-full mx-auto relative">
+
+//         <div className="w-fit border-4 border-[#77421A] rounded-full mx-auto">
+//           <div className="flex  flex-col   mx-auto justify-center py-3 md:py-5 px-6   lg:gap-0  landing-container rounded max-h-[18rem] aspect-square   max-w-[18rem] border-4 border-[#DDC47E]">
+//             <div className="flex flex-col  text-center md:pb-1 ">
+//               <p className="block  font-sans text-3xl md:text-2xl font-medium ">4.9</p>
+//               <div className="flex justify-center  ">
+//                 <RiStarSFill className="text-rilke-red text-2xl" />
+//                 <RiStarSFill className="text-rilke-red text-2xl" />
+//                 <RiStarSFill className="text-rilke-red text-2xl" />
+//                 <RiStarSFill className="text-rilke-red text-2xl" />
+//                 <RiStarSFill className="text-rilke-red text-2xl" />
+//               </div>
+
+//               <p className=" font-serif text-2xl md:text-xl">reviews</p>
+//             </div>
+//             <div className=" text-center  text-[#282727] text-base md:text-base max-w-[30rem]  md:max-w-[50rem] mx-auto  ">
+//               <p className=" py-4">
+//                 &apos;La inauguración del café propició el encuentro del arte,
+//                 la música y la gastronomía&apos;
+//               </p>
+//               <a
+//                 href="https://cosas.pe/vida-social/68622/cafe-rilke/"
+//                 target="_blank"
+//                 className="block w-fit mx-auto pb-2"
+//                 rel="noopener noreferrer"
+//               >
+//                 <img
+//                   src="/assets/cosas/cosas.png"
+//                   alt=""
+//                   className=" h-8 md:h-10 animation-opacity-infinite mx-auto    "
+//                 />
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Marketing;
